11a4f95d05828e5e7bfbd61285f0e13209c39d95
khenryjoe <joseph.ketterer@gmail.com>
2019-07-08 20:53:49 +0100

    df

--- classes/Controlview.php
+++ classes/Controlview.php
@@ -5,6 +5,16 @@ class Controlview extends Model
     // Function to get index page information
     protected function getIndex()
     {
+
     }
+
+
+    protected function getForm()
+    { 
+        $headerhtml = './templates/header.html';
+        $header = file_get_contents($headerhtml);
+
+    }
+   
 }
 ?>
