515ed97d7900500dd57a6b7b2ede8c33d34338e5
khenryjoe <joseph.ketterer@gmail.com>
2019-07-09 16:22:16 +0100

    add form

--- views/single.php
+++ views/single.php
@@ -18,19 +18,8 @@ $form = new Controlview($config);
 
 $form -> printForm();
 
-?>
-<h1>Upload a file:</h1>	
-<form enctype="multipart/form-data" action="<?php echo htmlentities($_SERVER['REQUEST_URI'], ENT_QUOTES, 'UTF-8'); ?>" method="post">
-    <div>
-        <label for="fileinput">Upload a file:</label>
-        <!-- "name" of input (userfile) will be the "key" in $_FILES -->
-        <input name="userfile" type="file" id="fileinput" />
-    </div>
-    <div>
-        <input type="submit" value="Upload File" name="singlefileupload" />
-    </div>
-</form>
-<?php
+
+require './includes/imageform.php';
 
 $form -> submitForm();
 
