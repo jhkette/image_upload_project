c6e87a80a92477d0e420fb21c1824a7ed67c62a6
khenryjoe <joseph.ketterer@gmail.com>
2019-07-17 13:38:36 +0100

    sql

--- createTable.sql
+++ createTable.sql
@@ -6,3 +6,8 @@ CREATE TABLE `photos3`
 `title` TEXT NOT NULL , 
 `description_p` TEXT NOT NULL, 
 PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+-- I am adding the lead images, and thumb images to the table as columns. 
+-- This simply means I have to write less code - i don't need to add text to a main filename to make thumbnail appear etc.
+-- I also feel it prevents errors - the filenames are saved to the database after I add the files to their individual
+-- folders. 
