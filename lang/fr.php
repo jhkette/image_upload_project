498415a9f1532c61c119e4224ba6a91dc5ddb666
khenryjoe <joseph.ketterer@gmail.com>
2019-07-29 13:56:17 +0100

    language error

--- lang/fr.php
+++ lang/fr.php
@@ -14,7 +14,14 @@
     '404_heading' => "404 Erreur - Page non trouvée",
     'config_empty' =>"Config details not found",
     'mysql-error' =>"There has been a mysql error",
-    'photo-number' =>'This is not a valid photo number'
+    'photo-number' =>'This is not a valid photo number',
+    'photo-absent' =>'This is not an image we have in our collection',
+  'jpg-error' =>'This file is not the correct mime type. Only jpg file should be uploaded',
+  'jpg-ext'=>'This is not the correct file extension',
+  'process-err'=>'This is not a file that can be processed',
+  'name-err'=>'This image name is already in use',
+  'json-find' => 'This photo is not in the database',
+  'json-err' => 'An error has occured whilst encoing the data to JSON'
   );
 
 ?>
